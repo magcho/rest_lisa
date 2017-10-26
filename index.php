79e214b1853c7ae0afdf45ded70a8755f2d501af
MagCho <gg845@hotmail.co.jp>
2017-10-27 01:26:49 +0900

    [update]analytics 注意書き 表現を曖昧に修正

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-108743341-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-108743341-1');
+</script>
+
+    <meta charset="utf-8">
+    <title>rest_lisa</title>
+  </head>
+  <body>
 <?php
 require_once('./lib.php');
 
@@ -10,25 +27,25 @@ $i = rest_lisa();
 if($i[0] == 'true'){
   //時間処理
   if($hour < 6){
-    echo "<h1>学校 is なくなるかも６時まで待とう</h1><br /><br />## 勝因<br />";
+    echo "<h1>学校 is たぶんなくなるかも６時まで待とう</h1><br /><br />## 勝因<br />";
     foreach ($i[1] as $key => $value) {
       echo " - ".$value."<br />";
     }
     exit();
   }elseif($hour < 8){
-    echo "<h1>学校(1限) is ない</h1><br /><br />## 勝因<br />";
+    echo "<h1>学校(1限) is たぶんない</h1><br /><br />## 勝因<br />";
     foreach ($i[1] as $key => $value) {
       echo " - ".$value."<br />";
     }
     exit();
   }elseif($hour < 11){
-    echo "<h1>学校(1,2限) is ない</h1><br /><br />## 勝因<br />";
+    echo "<h1>学校(1,2限) is たぶんない</h1><br /><br />## 勝因<br />";
     foreach ($i[1] as $key => $value) {
       echo " - ".$value."<br />";
     }
     exit();
   }elseif($hour >= 11){
-    echo "<h1>学校 is ない(今日は休み)</h1><br /><br />## 勝因<br />";
+    echo "<h1>学校 is たぶんない(今日は休み)</h1><br /><br />## 勝因<br />";
     foreach ($i[1] as $key => $value) {
       echo " - ".$value."<br />";
     }
@@ -41,3 +58,12 @@ if($i[0] == 'true'){
 }else{
   echo "<h1>学校 is たぶんある</h1>";
 }
+
+?>
+
+<p>※MagChoが作ったものでいかなる損害があっても責任とかとりません、自己責任でどうぞ。 情報は簡易的なものです、必ず複数かつ正確な情報を確認してください。</p>
+<p>Google Analytics導入してます。</p>
+<p>APIもあるよ -> <a href="http://magcho.webcrow.jp/rest_lisa/api/v1/">API</a></p>
+<p>詳しくは -> <a href="https://github.com/magcho/rest_lisa">Github</a></p>
+</body>
+</html>
